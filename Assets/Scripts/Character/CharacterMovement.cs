0cc8b426e01c66ee8167066578333241036dfca9
Heasds <antonruckov@gmail.com>
2023-02-28 00:35:17 +0300

    Добавлена логика передвижения персонажа
    Поправил анимации, написал и настроил логику передвижения персонажа.

--- Assets/Scripts/Character/CharacterMovement.cs
+++ Assets/Scripts/Character/CharacterMovement.cs
@@ -1,6 +1,38 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class CharacterMovement : MonoBehaviour
 {
+    public Animator characterAnimator;
+    public Transform characterTr;
+    public NavMeshAgent characterNavMesh;
+    public Joystick joystick;
 
+    [SerializeField] private float _MoveSpeed;
+    [SerializeField] private float _RotateSpeed;
+
+    private Vector3 _InputVector;
+
+    private void FixedUpdate()
+    {
+        CharacterMove();
+        CharacterRotate();
+    }
+
+    private void CharacterMove()
+    {
+        _InputVector.x = joystick.Horizontal;
+        _InputVector.z = joystick.Vertical;
+
+        characterNavMesh.Move(_InputVector * _MoveSpeed);
+        characterAnimator.SetBool("IsRun", _InputVector != Vector3.zero);
+    }
+
+    private void CharacterRotate()
+    {
+        if (_InputVector == Vector3.zero) return;
+
+        characterTr.rotation = Quaternion.Lerp(characterTr.rotation,
+            Quaternion.LookRotation(new Vector3(_InputVector.x, 0, _InputVector.z)), _RotateSpeed);
+    }
 }
